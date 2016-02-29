5e1341c085afb579395e2008d5c7a623366cb88e
bradvandyk <bradvandyk@gmail.com>
2016-02-29 11:42:53 -0500

    Added files via upload

---
+++ swiftErrorWarningGenerator.sh
@@ -0,0 +1,5 @@
+#echo "/Users/bradley.vandyk/code/test/lintTest/lintTest/main.swift:11: warning: Trailing Newline Violation: Files should have a single trailing newline. (trailing_newline)"
+#echo "/Users/bradley.vandyk/code/test/lintTest/lintTest/main.swift:12: error: why are you using this code?"
+echo "error: generic"
+#echo "/Users/bradley.vandyk/code/test/lintTest/lintTest/main.swift:11: warning: hieeee"
+exit 1
