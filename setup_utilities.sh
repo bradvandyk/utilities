6edb9a528faf014df9e174340167865ecad8c58f
Bradley Van Dyk <bradley.vandyk@angieslist.com>
2016-03-10 16:52:12 -0500

    add peruser settings

--- setup_utilities.sh
+++ setup_utilities.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cp ./bash_profile ~/.bash_profile
-#cp ./gitconfig ~/.gitconfig
+cp ./gitconfig ~/.gitconfig
+
+# update specific variables
+email=$(cat gitconfig.emailaddress)
+echo $email
+sed -i -e "s/{gitconfig.emailaddress}/$email/g" ~/.gitconfig
+
 #source ~/.bash_profile
 #exec bash # used to source ~/.bash_profile, not the best but no alternative
