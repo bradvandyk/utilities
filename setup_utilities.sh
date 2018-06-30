31d8f1c28cbf3202f9f23f787438cbe28ba44c16
Bradley Van Dyk <bradley.vandyk@angieslist.coms>
2018-06-30 19:51:38 -0400

    fix email address 2

--- setup_utilities.sh
+++ setup_utilities.sh
@@ -3,8 +3,8 @@ cp ./bash_profile ~/.bash_profile
 cp ./gitconfig ~/.gitconfig
 
 # update specific variables
-email=$(cat gitconfig.emailaddress)
-sed -i -e "s/{gitconfig.emailaddress}/$email/g" ~/.gitconfig
+email=$(cat gcf.emailaddress)
+sed -i -e "s/{gcf.emailaddress}/$email/g" ~/.gitconfig
 
 #source ~/.bash_profile
 #exec bash # used to source ~/.bash_profile, not the best but no alternative
