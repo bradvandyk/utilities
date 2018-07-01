44887d0695c1619940ae020c4169cea755573b79
Bradley Van Dyk <bradley.vandyk@homeadvisor.com>
2018-06-30 20:06:39 -0400

    fix copy to use right gcf

--- copygitconfigfromProfileToHere.sh
+++ copygitconfigfromProfileToHere.sh
@@ -5,4 +5,4 @@ cp ~/.gitconfig ./gitconfig
 email=$(cat gcf.emailaddress)
 echo $email
 #sed -i -e "s/{gcf.emailaddress}/$email/g" ~/.gitconfig
-sed -i -e "s/$email/{gcf.emailaddress}/g" ~/.gitconfig
+sed -i -e "s/$email/{gcf.emailaddress}/g" ./gitconfig
