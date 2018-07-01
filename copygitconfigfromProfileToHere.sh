694b82cb154229aa16568b5c43121fc713163010
Bradley Van Dyk <{gcf.emailaddress}>
2018-06-30 20:04:55 -0400

    update copy script

--- copygitconfigfromProfileToHere.sh
+++ copygitconfigfromProfileToHere.sh
@@ -2,5 +2,7 @@
 cp ~/.gitconfig ./gitconfig
 
 # update specific variables
-#email=$(cat emailaddress)
+email=$(cat gcf.emailaddress)
+echo $email
 #sed -i -e "s/{gcf.emailaddress}/$email/g" ~/.gitconfig
+sed -i -e "s/$email/{gcf.emailaddress}/g" ~/.gitconfig
